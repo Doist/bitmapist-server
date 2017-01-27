c55600e438ecd82fcdd5ba0edf37bb11c36fe59f
Artyom Pervukhin <artyom@evasive.ru>
2017-01-27 17:23:00 +0300

    Add some tests

---
+++ internal/bitmapist/bitmapist_test.go
@@ -0,0 +1,121 @@
+package bitmapist
+
+import (
+	"bufio"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/artyom/red"
+	"github.com/artyom/resp"
+)
+
+func TestServer(t *testing.T) {
+	cf, cleanup := newServer(t)
+	defer cleanup()
+	conn := cf()
+	defer conn.Close()
+	rd := bufio.NewReader(conn)
+	checkResponse(t, "setbit foo 10 1", int64(0), rd, conn)
+	checkResponse(t, "setbit foo 10 1", int64(1), rd, conn)
+	checkResponse(t, "setbit foo 7 1", int64(0), rd, conn)
+	checkResponse(t, "setbit bar 7 1", int64(0), rd, conn)
+	checkResponse(t, "setbit bar 20 1", int64(0), rd, conn)
+	checkResponse(t, "setbit bar 20 0", int64(1), rd, conn)
+	checkResponse(t, "setbit baz 8 1", int64(0), rd, conn)
+
+	checkResponse(t, "get foo", "\x01 ", rd, conn)
+	checkResponse(t, "get bar", "\x01", rd, conn)
+	checkResponse(t, "get baz", "\x00\x80", rd, conn)
+
+	checkResponse(t, "bitop or dst foo bar", int64(2), rd, conn)
+	checkResponse(t, "bitcount dst", int64(2), rd, conn)
+	checkResponse(t, "getbit dst 10", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 7", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 0", int64(0), rd, conn)
+	checkResponse(t, "get dst", "\x01 ", rd, conn)
+
+	checkResponse(t, "bitop xor dst foo bar", int64(1), rd, conn)
+	checkResponse(t, "bitcount dst", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 10", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 7", int64(0), rd, conn)
+	checkResponse(t, "get dst", "\x00 ", rd, conn)
+
+	checkResponse(t, "bitop xor dst foo bar baz", int64(2), rd, conn)
+	checkResponse(t, "bitcount dst", int64(2), rd, conn)
+	checkResponse(t, "getbit dst 10", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 8", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 7", int64(0), rd, conn)
+	checkResponse(t, "get dst", "\x00\xa0", rd, conn)
+
+	checkResponse(t, "bitop and dst foo bar", int64(1), rd, conn)
+	checkResponse(t, "bitcount dst", int64(1), rd, conn)
+	checkResponse(t, "getbit dst 10", int64(0), rd, conn)
+	checkResponse(t, "getbit dst 7", int64(1), rd, conn)
+	// this differs from redis which returns 2-byte response since it'd
+	// allocate array to fit longest source
+	checkResponse(t, "get dst", "\x01", rd, conn)
+}
+
+func checkResponse(t testing.TB, req string, respWanted interface{}, rd resp.BytesReader, wr io.Writer) {
+	if err := resp.Encode(wr, strings.Fields(req)); err != nil {
+		t.Fatal(err)
+	}
+	response, err := resp.Decode(rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(respWanted, response) {
+		t.Logf("op:\t%q", req)
+		t.Fatalf("mismatch:\n got:\t%#v\nwant:\t%#v", response, respWanted)
+	}
+}
+
+func newServer(t testing.TB) (fn clientConnFunc, cleanup func()) {
+	td, err := ioutil.TempDir("", "bitmapist-test-")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	srv, err := New(filepath.Join(td, "temp.db"))
+	if err != nil {
+		os.RemoveAll(td)
+		t.Fatal(err)
+		return
+	}
+	redsrv := red.NewServer()
+	srv.Register(redsrv)
+	unixSocket := filepath.Join(td, "bitmapist.sock")
+	ln, err := net.Listen("unix", unixSocket)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	go func() { redsrv.Serve(ln) }()
+	fn = func() net.Conn {
+		conn, err := net.DialTimeout("unix", unixSocket, time.Second)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := conn.SetDeadline(time.Now().Add(10 * time.Second)); err != nil {
+			t.Fatal(err)
+		}
+		return conn
+	}
+	cleanup = func() {
+		ln.Close()
+		if err := srv.Shutdown(); err != nil {
+			t.Error(err)
+		}
+		os.RemoveAll(td)
+	}
+	return fn, cleanup
+}
+
+type clientConnFunc func() net.Conn
