Doist/bitmapist-server

commit 5ebec30b2391fe6d23f8ce9dc64a24d3ef397140

Refactor getBitmap to merge two similar paths together

internal/bitmapist/bitmapist.go
@@ -148,7 +148,8 @@ func (s *Server) putBitmap(withLock bool, key string, bm *roaring.Bitmap, keepEx
 
 func (s *Server) getBitmap(key string, create, setDirty bool) (*roaring.Bitmap, error) {
 	nanots := time.Now().UnixNano()
-	if _, ok := s.keys[key]; !ok {
+	switch v, ok := s.cache[key]; {
+	case !ok || (ok && v.expire > 0 && nanots > v.expire): // not found or expired
 		if !create {
 			return nil, nil
 		}
@@ -159,22 +160,7 @@ func (s *Server) getBitmap(key string, create, setDirty bool) (*roaring.Bitmap, 
 			dirty: true,
 		}
 		return s.cache[key].b, nil
-	}
-	if v, ok := s.cache[key]; ok {
-		if v.expire > 0 && nanots > v.expire { // already expired
-			// TODO: refactor this block to merge it with similar
-			// one above
-			if !create {
-				return nil, nil
-			}
-			s.keys[key] = struct{}{}
-			s.cache[key] = cacheItem{
-				b:     roaring.NewBitmap(),
-				aTime: time.Now().Unix(),
-				dirty: true,
-			}
-			return s.cache[key].b, nil
-		}
+	case ok:
 		v.aTime = time.Now().Unix()
 		if !v.dirty && setDirty {
 			v.dirty = true

commit adfa665f73040e282f075a03513ad0a3c08fb339

Add test illustrating bug introduced by 5ebec30b239

internal/bitmapist/bitmapist_test.go
@@ -146,3 +146,60 @@ func newServer(t testing.TB) (fn clientConnFunc, cleanup func()) {
 }
 
 type clientConnFunc func() net.Conn
+
+func TestServerPersistence(t *testing.T) {
+	td, err := ioutil.TempDir("", "bitmapist-test-")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer os.RemoveAll(td)
+	newServerDirectory := func(t testing.TB) (fn clientConnFunc, cleanup func()) {
+		srv, err := New(filepath.Join(td, "temp.db"))
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+		redsrv := red.NewServer()
+		srv.Register(redsrv)
+		unixSocket := filepath.Join(td, "bitmapist.sock")
+		ln, err := net.Listen("unix", unixSocket)
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+		go func() { redsrv.Serve(ln) }()
+		fn = func() net.Conn {
+			conn, err := net.DialTimeout("unix", unixSocket, time.Second)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := conn.SetDeadline(time.Now().Add(10 * time.Second)); err != nil {
+				t.Fatal(err)
+			}
+			return conn
+		}
+		cleanup = func() {
+			ln.Close()
+			if err := srv.Shutdown(); err != nil {
+				t.Error(err)
+			}
+		}
+		return fn, cleanup
+	}
+	cf, cleanup := newServerDirectory(t)
+	conn := cf()
+	rd := bufio.NewReader(conn)
+	checkResponse(t, "setbit foo 10 1", int64(0), rd, conn)
+	checkResponse(t, "setbit foo 7 1", int64(0), rd, conn)
+	conn.Close()
+	cleanup()
+
+	// second launch should load data from disk
+	cf, cleanup = newServerDirectory(t)
+	defer cleanup()
+	conn = cf()
+	rd = bufio.NewReader(conn)
+	defer conn.Close()
+	checkResponse(t, "get foo", "\x01 ", rd, conn)
+}